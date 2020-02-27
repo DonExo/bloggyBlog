b3c1ba9bd63505d34125fd374cd558bf2a2af8a1
Dimitriovski <donfrozex@gmail.com>
2020-02-27 21:06:10 +0100

    Added basic custom User

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'is_staff']
+    list_filter = ['is_staff']
+    search_fields = ['first_name', 'last_name', 'email']
+
+
+admin.site.register(User, UserAdmin)
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255, blank=False)
+    last_name = models.CharField(max_length=255, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+
+    def __str__(self):
+        return self.get_full_name()
